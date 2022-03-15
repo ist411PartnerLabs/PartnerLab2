b7a96ec73a428d5ecc1be742fafb6dba09cb0831
Ethan Niemeyer <ejn5196@psu.edu>
2022-03-15 14:23:18 -0400

    Changes

---
+++ script.js
@@ -0,0 +1,49 @@
+let data;
+populateSports();
+
+
+function getSportSelected()
+{
+    let selectedSport = document.querySelector("#sportList").selectedIndex;
+    console.log(selectedSport);
+
+    document.querySelector("#sportDescription").innerHTML = "";
+
+    let selectedDescription = data[selectedItem].description;
+    document.querySelector("#sportDescription").innerHTML += selectedDescription;
+
+    console.log(selectedDescription); 
+}
+
+
+function populateSports()
+{
+    const request = new XMLHttpRequest();
+
+    request.open("GET",  "https://api.the-odds-api.com/v4/sports/?apiKey=bfbf9378d9a5c0f7d7889dab28d7b64a", true);
+
+    request.onload = function() {
+        data = JSON.parse(this.response); // request.response
+
+        if(request.status == 200)
+        {
+            console.log("Response OK");
+            
+            data.forEach(
+                sport => 
+                {
+                    let sportOption = document.createElement('option');
+                    let sportText = document.createTextNode(sport.title);
+                    sportOption.appendChild(sportText);
+                    document.querySelector("#sportList").appendChild(sportOption);
+                }
+            );
+        }
+        else
+        {
+            console.log(`Error occured: Status: ${request.status}`);
+        }
+    };
+
+    request.send();
+}
