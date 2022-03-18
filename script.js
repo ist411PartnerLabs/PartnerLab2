b0ebc45d0ceba7e4024afb21a40418179eabe85f
Ethan Niemeyer <ejn5196@psu.edu>
2022-03-18 14:56:54 -0400

    Link:

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function getSportSelected()
     console.log(selectedSport);
 
     const request = new XMLHttpRequest();
-    request.open("GET",  "https://api.the-odds-api.com/v4/sports/"+ selectedSport.id +"/scores/?daysFrom=1&apiKey=bfbf9378d9a5c0f7d7889dab28d7b64a", true);
+    request.open("GET",  "https://api.the-odds-api.com/v4/sports/basketball_nba/scores/?daysFrom=1&apiKey=bfbf9378d9a5c0f7d7889dab28d7b64a", true);
 
     let selectedDescription = data[selectedSport].id;
     document.querySelector("#sportDescription").innerHTML += selectedDescription;
